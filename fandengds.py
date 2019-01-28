62ff1f0a85eb3dbe437e17378ef1337baa2a4c46
༺ Yuri ༒ Boyka ༻ <lilung.chao@gmail.com>
2019-01-28 19:14:05 +0800

    add mongodb operation

--- fandengds.py
+++ fandengds.py
@@ -9,9 +9,14 @@ import urllib3
 import requests
 import io
 import sys
+
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+# import pymongo
+import pymysql
 
 url = "http://dushu.fandengds.com/cy/index.html"
+global mydb
+global cursor
 
 
 def parseContent(content):
@@ -19,25 +24,81 @@ def parseContent(content):
     soup = BeautifulSoup(content, "lxml")
     target = soup.find(name="div", attrs={'class': {"book_ls"}})
     li_list = target.find_all('li')
+    books = []
     for li in li_list:
         a_tag = li.find('a')
         if a_tag:
             href = "http://dushu.fandengds.com" + a_tag.attrs.get("href")
             title = a_tag.attrs.get("title")
             book_cover = "http://dushu.fandengds.com" + \
-                a_tag.find("img").attrs.get('src')
+                         a_tag.find("img").attrs.get('src')
             book_name = a_tag.find("div", class_="bkname text-nowrap")
             book_author = a_tag.find("div", class_="bkauthor text-nowrap")
             book_intro = a_tag.find("div", class_="bkintro text-nowrap")
-            print("[书名]: "+book_name.text+"\n[作者]: " +
-                  book_author.text+"\n[简介]: "+book_intro.text + "\n[链接]: " + href + "\n[封面]: " + book_cover + "\n[标题]: "+title + "\n")
+            print("[书名]: " + book_name.text + "\n[作者]: " +
+                  book_author.text + "\n[简介]: " + book_intro.text + "\n[链接]: " + href + "\n[封面]: " + book_cover + "\n[标题]: " + title + "\n")
+            book = {}
+            book["title"] = title
+            book["author"] = book_author.text
+            book["cover"] = book_cover
+            book["name"] = book_name.text
+            book["intro"] = book_intro.text
+            books.append(book)
+    # saveToMongoDB(books)
+    # updateBookInfo("联盟", "盟联")
+    # createTable()
+
+#
+# def createTable():
+#     cursor.execute("DROP TABLE IF EXISTS BOOK")
+#     # 使用预处理语句创建表
+#     sql = """CREATE TABLE BOOK (
+#          TITLE  CHAR(255) NOT NULL,
+#          AUTHOR  CHAR(255),
+#          COVER CHAR(255),
+#          NAME CHAR(255),
+#          INTRO CHAR(255) )CREATE TABLE 'BOOK' ('TITLE'  CHAR(255) NOT NULL,  'AUTHOR'  CHAR(255),  'COVER' CHAR(255),  'NAME' CHAR(255),'INTRO' CHAR(255) )""
+#     cursor.execute(sql)
+
+
+# MongoDB Operation
+def saveToMongoDB(books):
+    mycol = mydb["books"]
+    mycol.ensure_index([("title", 1)], unique="unique")
+    result = mycol.insert_many(books)
+    print(result.inserted_ids)
+
+
+def getAllBooks():
+    mycol = mydb["books"]
+    for book in mycol.find():
+        print(book)
+
+
+def deleteBook(name):
+    mycol = mydb["books"]
+    my_query = {"title": name}
+    mycol.delete_one(my_query)
+
+
+def queryBook(name):
+    mycol = mydb["books"]
+    for x in mycol.find({"title": name}):
+        print(x)
+
+
+def updateBookInfo(old_name, name):
+    mycol = mydb["books"]
+    myquery = {"title": old_name}
+    newvalue = {"$set": {"title": name}}
+    mycol.update_one(myquery, newvalue)
 
 
 def requestUrl(scrapy_url):
     # 获取数据
     response = requests.get(url)
     # 获取数据编码
-    print("[编码]: "+response.encoding + "\n[状态码]:" +
+    print("[编码]: " + response.encoding + "\n[状态码]:" +
           str(response.status_code) + "\n[内容]:\n" + response.text)
     # 将数据编码成utf-8编码
     response.encoding = 'utf-8'
@@ -49,9 +110,18 @@ def urllib3Request(scrapy_url):
     http = urllib3.PoolManager()
     # 通过request()方法创建一个请求：
     response = http.request('GET', scrapy_url)
-    print("[状态码]: "+str(response.status) + "\n[数据]: \n"+response.data.decode())
+    print("[状态码]: " + str(response.status) + "\n[数据]: \n" + response.data.decode())
     parseContent(response.data.decode())
 
 
 if __name__ == "__main__":
+    mydb = pymysql.connect("localhost", "root", "12345678", "fandengds")
+    cursor = mydb.cursor()
+    cursor.execute("SELECT VERSION()")
+    # 使用 fetchone() 方法获取单条数据.
+    data = cursor.fetchone()
+    print("Database version : %s " % data)
+    # MongoDB初始化
+    # myclient = pymongo.MongoClient("mongodb://localhost:27017")
+    # mydb = myclient["fandengds"]
     urllib3Request(url)
